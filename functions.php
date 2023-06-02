53d8eccd96c95384f4c133589801da09c919dbe2
Miguel Gil <miguelgilmartinez@gmail.com>
2023-06-02 11:16:31 +0200

    Iniciar-sesion does not redirect to Estudio

--- functions.php
+++ functions.php
@@ -581,11 +581,11 @@ function nodecharts_login_page()
             true
         );
     } else {
-        echo (apply_filters('wpml_current_language', null) == 'en'
+        echo (apply_filters('wpml_current_language', null) == 'en')
         ? '<h4>User logged in. Redirecting to the Studio</h4>
         <script type="text/javascript">console.log("Redirect");location.href="'.home_url('/en/studio').'"</script>' :
         '<h4>Sesión iniciada. Redirigiendo al Estudio</h4>
-        <script type="text/javascript">console.log("Redirect");location.href="'.home_url('/estudio').'"</script>');
+        <script type="text/javascript">console.log("Redirect");location.href="'.home_url('/estudio').'"</script>';
     }
 }
 add_shortcode('nodecharts-login-page', 'nodecharts_login_page');
