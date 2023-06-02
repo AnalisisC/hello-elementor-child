010cd48bc0ec18893ff0406837f30a9410241018
Miguel Gil <miguelgilmartinez@gmail.com>
2023-06-02 20:44:42 +0200

    Input form login fixed

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,3 +2,4 @@ jQuery('#user_login').attr('type', 'email').attr('placeholder', 'Email').addClas
 jQuery('#user_pass').attr('placeholder', 'Password').addClass('elementor-field elementor-size-md  elementor-field-textual').attr('required', 'true');
 jQuery('#wp-submit').addClass('elementor-button-link elementor-button elementor-size-lg');
 jQuery("a[href*='lost']").remove();
+jQuery("#loginform").attr('action', '/wp-login.php');
