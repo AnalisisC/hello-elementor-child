9926d4bd6d43b780de2a0112bd6c3654b0d9780d
Miguel Gil <miguelgilmartinez@gmail.com>
2023-06-04 19:00:35 +0200

    Lost password not removed by jquery

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,5 @@
 jQuery('#user_login').attr('type', 'email').attr('placeholder', 'Email').addClass('elementor-field elementor-size-md  elementor-field-textual').attr('required', 'true');
 jQuery('#user_pass').attr('placeholder', 'Password').addClass('elementor-field elementor-size-md  elementor-field-textual').attr('required', 'true');
 jQuery('#wp-submit').addClass('elementor-button-link elementor-button elementor-size-lg');
-jQuery("a[href*='lost']").remove();
+//jQuery("a[href*='lost']").remove();
 jQuery("#loginform").attr('action', '/wp-login.php');
