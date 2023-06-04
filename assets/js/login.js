cb23dacc223532ca80618c199c200a5e2a6d7519
Miguel Gil <miguelgilmartinez@gmail.com>
2023-06-04 19:12:43 +0200

    login.js wrapped in window.onload

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,8 @@
 
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
     jQuery('#user_login').attr('type', 'email').attr('placeholder', 'Email').addClass('elementor-field elementor-size-md  elementor-field-textual').attr('required', 'true');
     jQuery('#user_pass').attr('placeholder', 'Password').addClass('elementor-field elementor-size-md  elementor-field-textual').attr('required', 'true');
     jQuery('#wp-submit').addClass('elementor-button-link elementor-button elementor-size-lg');
     jQuery("#loginform").attr('action', '/wp-login.php');
-}
+});
+
