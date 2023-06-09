b1dee8bd1ed9565544416c4989267601c295f417
Miguel Gil <miguelgilmartinez@gmail.com>
2023-06-09 14:06:29 +0200

    User and password error

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,6 +2,5 @@ document.addEventListener("DOMContentLoaded", function () {
     jQuery('#wp-submit').addClass('elementor-button-link elementor-button elementor-size-lg');
     jQuery('#user_login').attr('type', 'email').attr('placeholder', 'Email').addClass('elementor-field elementor-size-md elementor-field-textual').attr('required', 'true');
     jQuery('#user_pass').attr('placeholder', 'Password').addClass('elementor-field elementor-size-md elementor-field-textual').attr('required', 'true');
-    //jQuery("#loginform").attr('action', '/wp-login.php');
 });
 
